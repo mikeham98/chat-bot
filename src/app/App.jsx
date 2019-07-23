c81a203a0c02f62e8c6e9ee8ed1968e62d726381
Michael Hamilton <98mikeham@gmail.com>
2019-07-23 21:42:58 +0100

    setup positioning of message in list, will refactor to a classname from inline style at a later date

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,6 +7,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <MessagesList
+                    currentUserId={1}
                     messages={messagesJSON}
                 />
             </div>
--- src/app/components/common/Messages/MessagesList.jsx
+++ src/app/components/common/Messages/MessagesList.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import styles from '../../../../themes/messages.scss';
 import isPopulatedArray from "../../../util/isPopulatedArray";
 import Message from "./Message";
+import {left, right} from "../../../config/messages.config";
 
 export default class MessagesList extends React.PureComponent {
     returnMessages() {
-        const {messages} = this.props;
-        if(isPopulatedArray(messages)) {
+        const {messages, currentUserId} = this.props;
+        if (isPopulatedArray(messages)) {
             return messages.map(message => {
+                const position = currentUserId === message.createdBy.userId ? right : left;
                 return (
-                    <Message
-                        body={message.body}
-                    />
+                    <div style={{alignSelf: position === left ? 'flex-start' :'flex-end'}}>
+                        <Message
+                            body={message.body}
+                        />
+                    </div>
                 );
             })
         }
@@ -18,7 +23,7 @@ export default class MessagesList extends React.PureComponent {
 
     render() {
         return (
-            <div>
+            <div className={styles.messageContainer}>
                 {this.returnMessages()}
             </div>
         )
