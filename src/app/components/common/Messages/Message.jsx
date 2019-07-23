e0f86b2b045e11d131db5204107e7d7a69ea8fa8
Michael Hamilton <98mikeham@gmail.com>
2019-07-23 22:10:26 +0100

    add initial styling to message/message list components

--- src/app/components/common/Messages/Message.jsx
+++ src/app/components/common/Messages/Message.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../../../../themes/messages.scss';
 
 const Message = ({body}) => {
     return (
-        <div>
+        <div className={styles.message}>
             {body}
         </div>
     );
--- src/app/components/common/Messages/MessagesList.jsx
+++ src/app/components/common/Messages/MessagesList.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from '../../../../themes/messages.scss';
 import isPopulatedArray from "../../../util/isPopulatedArray";
 import Message from "./Message";
 import {left, right} from "../../../config/messages.config";
 
 export default class MessagesList extends React.PureComponent {
+    returnMessageClassName(position) {
+        const classNames = [styles.messageWrapper];
+        if(position === right) {
+            classNames.push(styles.rightMessage);
+        }else {
+            classNames.push(styles.leftMessage);
+        }
+        return classnames(classNames);
+    }
+
     returnMessages() {
         const {messages, currentUserId} = this.props;
         if (isPopulatedArray(messages)) {
             return messages.map(message => {
                 const position = currentUserId === message.createdBy.userId ? right : left;
                 return (
-                    <div style={{alignSelf: position === left ? 'flex-start' :'flex-end'}}>
+                    <div className={this.returnMessageClassName(position)}>
                         <Message
                             body={message.body}
                         />
