2ca5f06ca5b22ff712646671abc73de590419c6b
Michael Hamilton <98mikeham@gmail.com>
2019-07-25 01:38:50 +0100

    add spinner loader on lazy load of components

--- src/app/layout/ChatBotLayout.jsx
+++ src/app/layout/ChatBotLayout.jsx
@@ -1,7 +1,8 @@
 import React, {Suspense} from 'react';
-import messagesReducer from "../reducers/messages/index.reducers";
-import store from "../store";
 import styles from '../../themes/components/layout.scss';
+import Spinner from "../components/common/Spinner";
+import store from "../store";
+import messagesReducer from "../reducers/messages/index.reducers";
 
 const ChatBotContainer = React.lazy(() =>
     import(/* webpackChunkName: "MESSAGES_LIST_CONTAINER" */ "../containers/ChatBot/index")
@@ -14,8 +15,12 @@ const ChatBotContainer = React.lazy(() =>
 export default class ChatBotLayout extends React.Component {
     render() {
         return (
-            <Suspense fallback={<div>Loading...</div>}>
-                    <ChatBotContainer/>
+            <Suspense fallback={(
+                <div className={styles.centerChatBotSpinner}>
+                    <Spinner/>
+                </div>
+            )}>
+                <ChatBotContainer/>
             </Suspense>
         )
     }
--- src/app/layout/ConversationPanelLayout.jsx
+++ src/app/layout/ConversationPanelLayout.jsx
@@ -1,7 +1,8 @@
 import React, {Suspense} from 'react';
 import SideBar from "../components/common/SideBar";
-import conversationsReducer from "../reducers/conversations/index.reducers";
+import Spinner from "../components/common/Spinner";
 import store from "../store";
+import conversationsReducer from "../reducers/conversations/index.reducers";
 
 const ConversationsListContainer = React.lazy(() =>
     import(/* webpackChunkName: "CONVERSATIONS_LIST_CONTAINER" */ "../containers/Conversations/ConversationsListContainer")
@@ -14,7 +15,7 @@ export default class ConversationPanelLayout extends React.Component {
     render() {
         return (
             <SideBar title='Messages'>
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<Spinner/>}>
                     <ConversationsListContainer/>
                 </Suspense>
             </SideBar>
